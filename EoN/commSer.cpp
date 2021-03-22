a05ce25343d7252e8b737394b415bbb05945d19c
slametsampon <slamet@pon.co.id>
2021-03-22 14:23:48 +0700

    Work on EoN - CommSer

--- EoN/commSer.cpp
+++ EoN/commSer.cpp
@@ -19,20 +19,61 @@ void CommSer::attachModelParameter(AccessParam *accessParameter){
  
 
 void CommSer::sendValue(JsonObject valueJson){
+  // Send the JSON document over the "link" serial port
+  serializeJson(valueJson, Serial1);
 
 }
 
 void CommSer::sendParameter(JsonObject paramJson){
     //JsonObject.as<JsonObject>()  paramJson = _accessParameter->getJson();
+  serializeJson(paramJson, Serial1);
 
 }
 
 JsonObject CommSer::getParameter(){
-    JsonObject  paramJson;
 
-    return paramJson;
+  if (Serial1.available()) 
+    {
+      // Allocate the JSON document
+      // This one must be bigger than for the sender because it must store the strings
+      StaticJsonDocument<192> paramJson;
+
+      // Read the JSON document from the "link" serial port
+      DeserializationError err = deserializeJson(paramJson, Serial1);
+
+      if (err == DeserializationError::Ok) 
+        {
+          // Print the values
+          // (we must use as<T>() to resolve the ambiguity)
+          // Get a reference to the root object
+          JsonObject obj = paramJson.as<JsonObject>();
+
+          // Loop through all the key-value pairs in obj
+          Serial.println("\nKey - Value Pair");
+          String str;
+          for (JsonPair p : obj) {
+            str = p.key().c_str();
+            str = String (str + " : ");
+            str = String (str + p.value().as<String>());
+            Serial.println(str);
+          }    
+        return obj;
+        } 
+      else 
+        {
+          // Print error to the "debug" serial port
+          Serial.print("deserializeJson() returned ");
+          Serial.println(err.c_str());
+      
+          // Flush all bytes in the "link" serial port buffer
+          while (Serial1.available() > 0)
+            Serial1.read();
+        }
+    }
 }
 
 void CommSer::execute(){
+
+  this->getParameter();
     
 }
--- EoN/commSer.h
+++ EoN/commSer.h
@@ -2,7 +2,78 @@
   commSer.h - Library untuk commSer
   Ini adalah bagian dari MVC (Model View Control) pattern design
   Dibuat Oleh : Sam March 22, 2021
+
+? value
+StaticJsonDocument<48> doc;
+
+doc["id"] = "Smoke-1";
+doc["value"] = 51.5;
+
+serializeJson(doc, output);
+
+? Stream& input;
+
+StaticJsonDocument<48> doc;
+
+DeserializationError error = deserializeJson(doc, input);
+
+if (error) {
+  Serial.print(F("deserializeJson() failed: "));
+  Serial.println(error.f_str());
+  return;
+}
+
+const char* id = doc["id"]; // "Smoke-1"
+float value = doc["value"]; // 51.5
+
+? Parameter
+{
+  "id":"Smoke-1",
+  "unit":"%",
+  "value":51.5,
+  "highRange":100.0,
+  "lowRange":0.0,
+  "highLimit":80.0,
+  "lowLimit":40.0,
+  "increment":1.1
+  }
+
+StaticJsonDocument<96> doc;
+
+doc["id"] = "Smoke-1";
+doc["unit"] = "%";
+doc["value"] = 51.5;
+doc["highRange"] = 100;
+doc["lowRange"] = 0;
+doc["highLimit"] = 80;
+doc["lowLimit"] = 40;
+doc["increment"] = 1.1;
+
+serializeJson(doc, output);
+
+? Stream& input;
+
+StaticJsonDocument<192> doc;
+
+DeserializationError error = deserializeJson(doc, input);
+
+if (error) {
+  Serial.print(F("deserializeJson() failed: "));
+  Serial.println(error.f_str());
+  return;
+}
+
+const char* id = doc["id"]; // "Smoke-1"
+const char* unit = doc["unit"]; // "%"
+float value = doc["value"]; // 51.5
+int highRange = doc["highRange"]; // 100
+int lowRange = doc["lowRange"]; // 0
+int highLimit = doc["highLimit"]; // 80
+int lowLimit = doc["lowLimit"]; // 40
+float increment = doc["increment"]; // 1.1
+
 */
+
 #ifndef commSer_h
 #define commSer_h
 
