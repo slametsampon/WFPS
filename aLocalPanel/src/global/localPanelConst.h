3d3fd655362bc55eff4a6edc05ea5e0332ea8cab
slametsampon <slamet@pon.co.id>
2021-04-27 08:33:47 +0700

    update debounce time to 150

--- aLocalPanel/src/global/localPanelConst.h
+++ aLocalPanel/src/global/localPanelConst.h
@@ -58,9 +58,9 @@ All constants for local panel
 
     const int ADC_MAX = 1023;//10 bits
     const int ALFA_EMA = 80;//AlfaEma filter
-    const int DEBOUNCING_TIME = 500;//milli second
+    const int DEBOUNCING_TIME = 150;//milli second
 
-    const int BLINK_NORMAL = 150;//milli second
+    const int BLINK_NORMAL = 500;//milli second
     const int BLINK_WARNING = 250;//milli second
     const int BLINK_ERROR = 125;//milli second
 
